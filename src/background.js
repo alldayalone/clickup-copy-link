f3ad2f24400c7f37224e77e41180683d4b25f825
Pavel Kalugin <p.kalugin@paralect.com>
2021-11-19 14:13:58 +0300

    Add multiple selected task copy, error handling

--- src/background.js
+++ src/background.js
@@ -3,6 +3,22 @@ chrome.action.onClicked.addListener(tab => {
     target: { tabId: tab.id },
     files: ['src/content.js']
   });
-  chrome.action.setBadgeBackgroundColor({ color: '#00bf00' });
-  chrome.action.setBadgeText({ text: "✓" });
+});
+
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.type === 'result' && message.result === 'success') {
+    chrome.action.setBadgeBackgroundColor({ color: '#00bf00' });
+    chrome.action.setBadgeText({ text: "✓" });
+    setTimeout(() => {
+      chrome.action.setBadgeText({ text: "" });
+    }, 3000);
+  }
+
+  if (message.type === 'result' && message.result === 'failure') {
+    chrome.action.setBadgeBackgroundColor({ color: '#eb0001' });
+    chrome.action.setBadgeText({ text: "✗" });
+    setTimeout(() => {
+      chrome.action.setBadgeText({ text: "" });
+    }, 3000);
+  }
 });
--- src/content.js
+++ src/content.js
@@ -1,16 +1,10 @@
-function getCardTitle() {
-  const taskNameElement = document.querySelector(".task-name__overlay");
-
-  return taskNameElement.textContent;
-}
-
-function buildHtmlLink(text, link) {
+function buildHtmlLink({ text, link }) {
   return `<a href="${link}"><span>${text}</span></a>`;
 }
 
-async function writeToClipboard(text, link) {
-  const textBlob = new Blob([text], { type: 'text/plain' });
-  const htmlBlob = new Blob([buildHtmlLink(text, link)], { type: 'text/html' });
+async function writeToClipboard(taskDetails) {
+  const textBlob = new Blob([taskDetails.map(c => c.text).join('\n')], { type: 'text/plain' });
+  const htmlBlob = new Blob([taskDetails.map(buildHtmlLink).join('<br>')], { type: 'text/html' });
 
   await navigator.clipboard.write([
     new ClipboardItem({
@@ -20,13 +14,62 @@ async function writeToClipboard(text, link) {
   ]);
 }
 
-async function main() {
-  const title = getCardTitle();
-  const [cardId] = location.pathname.split('/').reverse();
-  const text = `[${cardId}] ${title}`;
+function getTaskDetails() {
+  if (location.href.match(/app\.clickup\.com\/t/)) {
+    return getSingleTaskDetails();
+  }
+
+  return getMultipleTaskDetails();
+}
+
+function getSingleTaskDetails() {
+  const title = document.querySelector(".task-name__overlay").textContent;
+  const [taskId] = location.pathname.split('/').reverse();
+  const text = `[${taskId}] ${title}`;
   const link = location.href;
 
-  await writeToClipboard(text, link);
+  return [{ text, link }];
 }
+
+function getMultipleTaskDetails() {
+  const taskNodes = document.querySelectorAll('.lv-task_selected');
+
+  return Array.from(taskNodes).map(task => {
+    const title = task.querySelector(".cu-panel-board__clickable-name").textContent;
+    const taskId = task.querySelector('.cu-panel-board__footer_left-taskid').textContent;
+    const boardId = location.pathname.split('/')[1];
+    const text = `[${taskId}] ${title}`;
+    const link = `${location.origin}/t/${boardId}/${taskId}`;
+
+    return { text, link }
+  });
+}
+
+async function main() {
+  try {
+    const taskDetails = getTaskDetails()
+
+    if (!taskDetails.length) {
+      throw new Error('No tasks selected');
+    }
+
+    await writeToClipboard(taskDetails);
+  } catch (error) {
+    chrome.runtime.sendMessage({ type: 'result', result: 'failure' });
+
+    // Clipboard API is only available when document is focused
+    if (error.message.match('Document is not focused')) {
+      alert('Focus window and try again');
+    } else if (error.message.match('No tasks selected')) {
+      alert('Select some task or go to the task page');
+    } else {
+      console.error('Unexpected error from Clickup Copy extension:', error);
+    }
+
+    return;
+  }
+
+  chrome.runtime.sendMessage({ type: 'result', result: 'success' });
+};
 
 main();
